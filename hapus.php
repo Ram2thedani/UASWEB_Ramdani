2e723f9f860f57805bae16fb44337a49d8586832
Muhamad Ramdani <ram2thedani@gmail.com>
2024-06-15 17:19:33 +0700

    tambahkan komentar

--- hapus.php
+++ hapus.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
 include "config.php";
 
 $query = "DELETE FROM siswa WHERE RFID='$_GET[id]'";
 $data = $conn->prepare($query);
 $data->execute();
-
+//
 header("location: siswa.php");
-?>
